eb168968e556680df73fe0a54f87827ce3bf9a66
ykybkoft <ykybkoft2@gmail.com>
2024-07-19 14:06:19 +0900

    1st commit

---
+++ src/main/java/com/project/cookEats/CookEatsApplication.java
@@ -0,0 +1,13 @@
+package com.project.cookEats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookEatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cookEats/CookEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cookEats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
