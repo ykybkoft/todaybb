8bfab4c25bc7b223bca65b961f7af410de7d4515
ykybkoft <ykybkoft2@gmail.com>
2024-07-31 14:09:27 +0900

    Member table create

---
+++ src/main/java/com/project/cookEats/member/Member.java
@@ -0,0 +1,34 @@
+package com.project.cookEats.member;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@Entity
+@ToString
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    @Column(length = 20, unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true, length = 20)
+    private String nick;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String phone;
+    @Column(nullable = false)
+    private Date birth;
+    @Column(nullable = false)
+    private String gender;
+
+
+
+}
