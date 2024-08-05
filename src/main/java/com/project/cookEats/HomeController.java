7d703406e1e68f37e88216920a0007e79fc46de8
CatSayjjaek <jeonmyungse@naver.com>
2024-08-05 13:30:24 +0900

    homeHeader_and_css (#6)

---
+++ src/main/java/com/project/cookEats/HomeController.java
@@ -0,0 +1,12 @@
+package com.project.cookEats;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    String home(){
+        return "home.html";
+    }
+}
